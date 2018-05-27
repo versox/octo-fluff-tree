34fd8a5f02a54a8d78067bfc8fc1925e24e2856a
aravkbhatt <aravkbhatt@gmail.com>
2018-05-27 03:57:48 -0400

    Add files via upload

--- client/src/List.js
+++ client/src/List.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ListItem from './ListItem';
+import interact from 'interactjs';
 
 var listOfThings = ["world", "the", "hi"];
 
@@ -13,5 +14,42 @@ class List extends Component {
     return <div><ul>{items}</ul></div>
   }
 }
+
+
+
+      interact('.draggable')
+        .draggable({
+          // enable inertial throwing
+          inertia: true,
+          // keep the element within the area of it's parent
+          restrict: {
+
+            endOnly: true,
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+          },
+          // enable autoScroll
+          autoScroll: true,
+          // call this function on every dragmove event
+          onmove: dragMoveListener,
+        });
+
+        function dragMoveListener (event) {
+          var target = event.target,
+              // keep the dragged position in the data-x/data-y attributes
+              x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+              y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+          // translate the element
+          target.style.webkitTransform =
+          target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
+          // update the posiion attributes
+          target.setAttribute('data-x', x);
+          target.setAttribute('data-y', y);
+        }
+
+
+
 
 export default List;
